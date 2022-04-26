396ea014e63efab73064197f19f02350ed19adbe
mahirmiah <mahirmiah7@gmail.com>
2022-04-26 15:45:32 -0400

    Positioned Connect Toasts to top right

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -1,31 +1,37 @@
 import React, {useState, useRef } from 'react';
-import {Form,Button,Col,Row, Container, Toast, Stack} from 'react-bootstrap'
+import {Form,Button,Col,Row, Container, Toast, Stack,ToastContainer} from 'react-bootstrap'
 import Connect from '../pages/Connect'
 
 function Example() {
   const [show, setShow] = useState(false);
 
   return (
+
     <Row>
       <Col xs={8}>
-        <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
+         <ToastContainer position="top-end" className="p-3">
+
+        <Toast onClose={() => setShow(false)} show={show} delay={4000} autohide>
           <Toast.Header>
             <img
               src="holder.js/20x20?text=%20"
               className="rounded me-2"
               alt=""
             />
-            <strong className="me-auto">Bootstrap</strong>
+            <strong className="me-auto">Success!</strong>
             <small>Few seconds ago</small>
           </Toast.Header>
-          <Toast.Body>Success! You have sent an email.</Toast.Body>
+          <Toast.Body> You have sent an email.</Toast.Body>
         </Toast>
-      </Col>
-      <Col xs={6}>
-        <Button variant='primary' type='submit' onClick={(sendEmail) => setShow(true)}>Submit</Button>
+      </ToastContainer>
 
       </Col>
+
+      <Col xs={6}>
+        <Button variant='primary' type='submit' onClick={(sendEmail) => setShow(true)}>Submit</Button>
+      </Col>
     </Row>
+
   );
 }
 
